e27d57a69a090ece0a23e1065c9620d867d9037f
freddiehoward <howard.f@etoncollege.org.uk>
2024-06-06 09:49:00 +0100

    Initial Commit

---
+++ apitester/apitesterApp.swift
@@ -0,0 +1,17 @@
+//
+//  apitesterApp.swift
+//  apitester
+//
+//  Created by Freddie H on 06/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct apitesterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
